4edd1247603493353b2ad92856142e30cc2b4c38
KariSt1 <kari_steinn@outlook.com>
2020-10-05 13:08:26 +0000

    First commit

---
+++ src/main/java/is/hi/hbv501g/dotoo/DoToo/DoTooApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.dotoo.DoToo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoTooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoTooApplication.class, args);
+	}
+
+}
