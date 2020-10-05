af01c50cc7ebd6dccf3428b49e0978f4151d2304
gudlaugagnes <gak16@hi.is>
2020-10-05 15:05:37 +0000

    prufa gudlaug

--- src/main/java/is/hi/hbv501g/dotoo/DoToo/DoTooApplication.java
+++ src/main/java/is/hi/hbv501g/dotoo/DoToo/DoTooApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DoTooApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DoTooApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DoTooApplication.class, args);
+        //commment try, muna að eyða
+    }
 
 }
