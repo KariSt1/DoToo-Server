d5a2d2276707fd9649057bebf39be6c03009e176
annamaria99 <ams58@hi.is>
2020-10-08 11:42:12 +0000

    'User entity'

--- src/main/java/is/hi/hbv501g/dotoo/DoToo/Entities/User.java
+++ src/main/java/is/hi/hbv501g/dotoo/DoToo/Entities/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class User {
@@ -12,6 +13,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String username;
 
+    @NotNull
     private String name;
+    @NotNull
     private String password;
 }
