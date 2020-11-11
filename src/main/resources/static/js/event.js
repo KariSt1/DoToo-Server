8b0a599d56a2702237cc7a458b98bbea55f2b944
annamaria99 <ams58@hi.is>
2020-11-11 22:50:57 +0000

    'End date getur ekki verið á undan start date semi komið'

--- src/main/resources/static/js/event.js
+++ src/main/resources/static/js/event.js
@@ -7,3 +7,17 @@ document.addEventListener('DOMContentLoaded', () => {
     start.setAttribute("value", time);
     end.setAttribute("value", time);
 });
+
+function invalidDateRange() {
+    jQuery(function($) {
+        var startDate = document.getElementById("start-date").value;
+        var endDate = document.getElementById("end-date").value;
+
+        if (Date.parse(endDate) <= Date.parse(startDate)) {
+            alert("End date should be greater than start date");
+            return;
+        }
+        $("#myForm").submit();
+    });
+
+}
